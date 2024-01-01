c9b4f7f222f5a76e7213c770706ca9eff5524087
ARAVINDH KUMAR S <115638359+Aravindh2003@users.noreply.github.com>
2024-01-01 12:35:19 +0530

    Create 41. First Missing Positive.java

---
+++ Java code/41. First Missing Positive.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        int b=1;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==b)
+            {
+               b++;
+            }
+        }
+        return b;
+         
+    }
+}

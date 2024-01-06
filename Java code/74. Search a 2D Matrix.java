39a40abc89b76511e34bd126764a8157a7f9ff5d
ARAVINDH KUMAR S <115638359+Aravindh2003@users.noreply.github.com>
2024-01-06 19:27:03 +0530

    Create 74. Search a 2D Matrix.java

---
+++ Java code/74. Search a 2D Matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i=0;
+        for(i=0;i<matrix.length;i++){
+            if(target>matrix[i][matrix[0].length-1])
+            continue;
+            else {
+                for(int j=0;j<matrix[0].length;j++){
+                    if(matrix[i][j]==target)
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}

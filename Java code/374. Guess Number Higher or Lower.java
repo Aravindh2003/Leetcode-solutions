8c87e1f70b3343911c1ac1fe9c4542ee211bf453
ARAVINDH KUMAR S <115638359+Aravindh2003@users.noreply.github.com>
2024-01-21 11:31:41 +0530

    Create 374. Guess Number Higher or Lower.java

---
+++ Java code/374. Guess Number Higher or Lower.java
@@ -0,0 +1,16 @@
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+       int s=1,e=n;
+       while(s<=e){
+           int m=s+(e-s)/2;
+           if(guess(m)==0)
+           return m;
+           else if(guess(m)==1)
+           s=m+1;
+           else
+           e=m-1;
+       }
+       return -1;
+
+    }
+}

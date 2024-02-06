b69fb3c9ca7f04384e2ca98cf977b232b79af565
ARAVINDH KUMAR S <115638359+Aravindh2003@users.noreply.github.com>
2024-02-06 17:37:15 +0530

    Create 49. Group Anagrams.java

---
+++ Daily challenge /49. Group Anagrams.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (int i=0; i<strs.length; i++) {
+            char[] count = new char[26];
+            for (int j=0; j<strs[i].length(); j++) {
+                char c = strs[i].charAt(j);
+                count[c - 'a']++;
+            }
+            String str = new String(count);
+            if (!map.containsKey(str)) {
+                map.put(str, new ArrayList<>());
+            }
+            map.get(str).add(strs[i]);
+        }
+        return new ArrayList<>(map.values());
+    }
+}

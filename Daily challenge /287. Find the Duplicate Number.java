69ce3ec60053c586cb37fc21d9713271e5659627
ARAVINDH KUMAR S <115638359+Aravindh2003@users.noreply.github.com>
2024-03-24 11:26:45 +0530

    Create 287. Find the Duplicate Number.java

---
+++ Daily challenge /287. Find the Duplicate Number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+    boolean[] b=new boolean[nums.length];
+    for(int num:nums){
+      if(b[num]){
+        return num;
+      }
+      b[num]=true;
+    }
+    return 0;
+    }
+}

41a7072375a8be05fb48faaddfd910935ff4f16e
ARAVINDH KUMAR S <115638359+Aravindh2003@users.noreply.github.com>
2024-05-18 18:39:33 +0530

    Create 979. Distribute Coins in Binary Tree.java

---
+++ Daily challenge /979. Distribute Coins in Binary Tree.java
@@ -0,0 +1,15 @@
+class Solution {
+    private int moves=0;
+    private int dfs(TreeNode node) {
+        if(node==null) 
+        return 0;
+        int left=dfs(node.left);
+        int right=dfs(node.right);
+        moves+=Math.abs(left)+Math.abs(right);
+        return node.val + left+right-1;
+    }
+    public int distributeCoins(TreeNode root) {
+         dfs(root);
+        return moves;
+    }
+}

7dffad82e3187eff84a8a79041185cdecc813510
ARAVINDH KUMAR S <115638359+Aravindh2003@users.noreply.github.com>
2024-05-20 17:50:59 +0530

    Create 1863. Sum of All Subset XOR Totals.java

---
+++ Daily challenge /1863. Sum of All Subset XOR Totals.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+          return subsets(nums,0,0);
+    }
+
+    private int subsets(int[] nums,int i,int curr){
+        if(i>=nums.length){
+            return curr;
+        }
+        return subsets(nums,i+1,nums[i]^curr)+subsets(nums,i+1,curr);
+    }
+}
